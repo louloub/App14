dc00ce1989b64845596a825bf6b8f930aea6b881
loulou <yourdj.france@gmail.com>
2018-12-04 17:44:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/haroldyourdj/app14/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.haroldyourdj.app14;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendar);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth)
+            {
+                String date = dayOfMonth + "/" + month + "/" + year ;
+                myDate.setText(date);
+            }
+        });
+    }
+}
